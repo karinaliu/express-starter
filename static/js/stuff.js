d6ed3f4fa70aa32bd7b20163fc78d7b3780808a2
Karina Liu <karinagliu@gmail.com>
2014-03-18 16:37:43 -0700

    Completed Lab 2

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -8,17 +8,24 @@ $('#say_hello').click(function() {
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
-  //WRITE CODE HERE
+  $('#houdini_text').hide();
 });
 
 $('#reappear').click(function() {
-  //WRITE CODE HERE
+  $('#houdini_text').show();
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
 // WRITE CODE HERE
-
+$('#tickle_me_pink').click(function() {
+  $('#tickled_text').css('color','Pink');
+});
 
 // Problem 4 (Greet Me) -----------------------------------------------------
 // WRITE CODE HERE
+$('#greet_me').click(function(val) {
+	haha = document.getElementById("my_name").value;
+	alert(haha);
+});
+ 
